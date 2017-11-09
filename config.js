fb4c3ef8ac41b523911ba513cf30d378fac330c3
Viktoras Kuznecovas <vku@dbml.dk>
2017-11-09 16:56:58 +0200

    slight change to the default optimization params

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const load = (hexoConfig) => {
     defaultParams: hexoConfig && hexoConfig.default_params
     ? hexoConfig.default_params.split(" ")
     : ('-resize 760000@ -filter Triangle -define filter:support=2 -unsharp 0.25x0.25+8+0.065 ' +
-     '-dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -interlace none ' +
+     '-dither None -posterize 136 -quality 85 -define jpeg:fancy-upsampling=off -interlace none ' +
      '-colorspace sRGB -strip -define png:compression-filter=5 -define png:compression-level=9 ' +
      '-define png:compression-strategy=1 -define png:exclude-chunk=all').split(" "),
     jpg: {
